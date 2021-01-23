012682b32c0698e91fc52624fd9e23b89c18b5b7
nowll <farukarslan011@gmail.com>
2021-01-23 22:15:10 +0300

    Use the properties

--- src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
+++ src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
@@ -8,12 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class ApplicationConfig {
 
-    @Value("Hello")
+    @Value("${app.greeting}")
     private String greeting;
+    @Value("${app.name}")
+    private String name;
 
     @Autowired
     private GreetingService greetingService;
@@ -28,11 +32,11 @@ public class ApplicationConfig {
 
     @Bean
     public OutputService outputService(){
-        return new OutputService(greetingService, timeService);
+        return new OutputService(name, greetingService, timeService);
     }
 
     @Bean
     public GreetingService greetingService(){
-        return new GreetingService("Hello");
+        return new GreetingService(greeting);
     }
 }
