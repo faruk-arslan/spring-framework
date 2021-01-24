1e1fce798f0c9075379cb464f22fdd041211d77c
nowll <farukarslan011@gmail.com>
2021-01-24 14:55:54 +0300

    Switch from Java configured app to Component scanned app.

--- src/main/java/com/frankmoley/lil/fid/service/OutputService.java
+++ src/main/java/com/frankmoley/lil/fid/service/OutputService.java
@@ -1,13 +1,25 @@
 package com.frankmoley.lil.fid.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OutputService {
 
-    private final String name;
+    @Value("${app.name}")
+    private String name;
     private final GreetingService greetingService;
     private final TimeService timeService;
 
-    public OutputService(String name, GreetingService greetingService, TimeService timeService){
-        this.name = name;
+    /**
+     * Spring knows that it should use this constructor and auto-wires the dependencies.
+     * (There's only one constructor and it's not the default. If it's default then there's
+     * nothing to inject anyway.)
+     * You can also add @Autowired annotation to state that explicitly.
+     */
+    @Autowired
+    public OutputService(GreetingService greetingService, TimeService timeService){
         this.greetingService = greetingService;
         this.timeService = timeService;
     }
